89bfa187e518c2888610849ec657ebeefabc9e9b
gitlayzer <gitlayzer@kudevops.cn>
2022-12-01 13:47:23 -0500

    add npm

--- src/org/library/build.groovy
+++ src/org/library/build.groovy
@@ -1,11 +1,17 @@
 package org.linrary
 
 def Build(buildType, buildShell) {
-    def buildTools = ["mvn": "maven", "ant": "ant", "gradle": "gradle"]
+    def buildTools = ["mvn": "maven", "ant": "ant", "gradle": "gradle", "npm": "npm"]
     
     println("当前构建类型为：${buildType}")
     
     buildHome = tool buildTools[buildType]
     
-    sh "${buildHome}/bin/${buildType} ${buildShell}"
+    if ("${buildType}" == "npm") {
+        sh """export NODE_HOME=${buildHome} 
+              export PATH=\$NODE_HOME/bin:\$PATH 
+              ${buildHome}/bin/${buildType} ${buildShell}"""
+    } else {
+        sh "${buildHome}/bin/${buildType} ${buildShell}"
+    }
 }
