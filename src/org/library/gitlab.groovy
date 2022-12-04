167aa837ffaa2174550d5f9c97012bb43f45d9a5
gitlayzer <gitlayzer@kudevops.cn>
2022-12-04 15:34:07 -0500

    fix gitlab code

--- src/org/library/gitlab.groovy
+++ src/org/library/gitlab.groovy
@@ -1,8 +1,10 @@
 package org.library
 
+
 def HttpRequest(requestType,requestUrl,requestBody) {
     def gitServer = "http://10.0.0.11/api/v4"
     withCredentials([string(credentialsId: 'gitlab-token', variable: 'gitlab')]) {
+      println("${gitlab}")
       result = httpRequest customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: "${gitlab}"]], 
           httpMode: requestType, 
           contentType: "APPLICATION_JSON", 
