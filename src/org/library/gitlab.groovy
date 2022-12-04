843da2ddaf850e2a490b0eb25de4cad387a63b8e
gitlayzer <gitlayzer@kudevops.cn>
2022-12-04 15:40:43 -0500

    fix gitlab code

--- src/org/library/gitlab.groovy
+++ src/org/library/gitlab.groovy
@@ -3,17 +3,20 @@ package org.library
 
 def HttpRequest(requestType,requestUrl,requestBody) {
     def gitServer = "http://10.0.0.11/api/v4"
+//    withCredentials([string(credentialsId: 'gitlab-token', variable: 'gitlab')]) {
+//      println("${gitlab}")
+//      result = httpRequest customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: "${gitlab}"]], 
+//          httpMode: requestType, 
+//          contentType: "APPLICATION_JSON", 
+//          consoleLogResponseBody: true, 
+//          ignoreSslErrors: true, 
+//          requestBody: requestBody, 
+//          url: "${gitServer}/${requestUrl}"
+//    }
+//    return result
     withCredentials([string(credentialsId: 'gitlab-token', variable: 'gitlab')]) {
-      println("${gitlab}")
-      result = httpRequest customHeaders: [[maskValue: true, name: 'PRIVATE-TOKEN', value: "${gitlab}"]], 
-          httpMode: requestType, 
-          contentType: "APPLICATION_JSON", 
-          consoleLogResponseBody: true, 
-          ignoreSslErrors: true, 
-          requestBody: requestBody, 
-          url: "${gitServer}/${requestUrl}"
+        println("${gitlab}")
     }
-    return result
 }
 
 // 更改提交状态
