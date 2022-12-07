2ab6afac324eb17c386cd82e5078b2e1a0a26419
gitlayzer <gitlayzer@kudevops.cn>
2022-12-07 04:28:23 -0500

    edit sonar code

--- src/org/library/sonarqube.groovy
+++ src/org/library/sonarqube.groovy
@@ -1,15 +1,13 @@
 package org.library
 
 // 代码扫描
-def SonarScan(projectName,projectDescription,projectPath){
+def SonarScan(projectName,projectDescription,projectPath,version){
     def sonarServer = "http://10.0.0.11:9000"
-    sonarDate = sh returnStdout: true, script: 'date +%Y%m%d%H%M%S'
-    sonarDate = sonarDate - "\n"
     sh """
     sonar-scanner -Dsonar.host.url=${sonarServer} \
               -Dsonar.projectKey=${projectName} \
               -Dsonar.projectName=${projectName} \
-              -Dsonar.projectVersion=${sonarDate} \
+              -Dsonar.projectVersion=${version} \
               -Dsonar.login=admin \
               -Dsonar.password=xxzx@123 \
               -Dsonar.ws.timeout=30 \
@@ -17,5 +15,8 @@ def SonarScan(projectName,projectDescription,projectPath){
               -Dsonar.sources="${projectPath}" \
               -Dsonar.sourceEncoding=UTF-8 \
               -Dsonar.java.binnaries=target/classes \
+              # 因为我的代码没有测试，所以这里就不加了，大家自己找的项目应该是有测试的记得加
+              # -Dsonar.java.test.binaries=target/test-classes \
+              # -Dsonar.java.surefire.report=target/surefire-reports
     """
 }
