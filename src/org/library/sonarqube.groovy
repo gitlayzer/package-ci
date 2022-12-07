df2667ec1fa0fcb9cfadf13886e0187c4b2b7d39
gitlayzer <gitlayzer@kudevops.cn>
2022-12-07 11:32:17 -0500

    edit sonarqube code

--- src/org/library/sonarqube.groovy
+++ src/org/library/sonarqube.groovy
@@ -1,16 +1,17 @@
 package org.library
 
 def SonarScan(projectName,projectDescription,projectPath,version){
-    withSonarQubeEnv("sonarqube")
-    def scannerHome = "/jenkins/software/sonar-scanner/"
-    sh """
-    ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
-                                     -Dsonar.projectName=${projectName} \
-                                     -Dsonar.projectVersion=${version} \
-                                     -Dsonar.ws.timeout=30 \
-                                     -Dsonar.projectDescription="${projectDescription}" \
-                                     -Dsonar.sources="${projectPath}" \
-                                     -Dsonar.sourceEncoding=UTF-8 \
-                                     -Dsonar.java.binnaries=target/classes \
-"""
+    withSonarQubeEnv("sonarqube"){
+        def scannerHome = "/jenkins/software/sonar-scanner/"
+        sh """
+        ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
+                                         -Dsonar.projectName=${projectName} \
+                                         -Dsonar.projectVersion=${version} \
+                                         -Dsonar.ws.timeout=30 \
+                                         -Dsonar.projectDescription="${projectDescription}" \
+                                         -Dsonar.sources="${projectPath}" \
+                                         -Dsonar.sourceEncoding=UTF-8 \
+                                         -Dsonar.java.binnaries=target/classes \
+    """
+    }
 }
