de7c9bc5c0d044eafa6aadcf57184899c224c12e
gitlayzer <gitlayzer@kudevops.cn>
2022-12-07 11:31:23 -0500

    edit sonarqube code

--- src/org/library/sonarqube.groovy
+++ src/org/library/sonarqube.groovy
@@ -1,16 +1,12 @@
 package org.library
 
-// 代码扫描
 def SonarScan(projectName,projectDescription,projectPath,version){
+    withSonarQubeEnv("sonarqube")
     def scannerHome = "/jenkins/software/sonar-scanner/"
-    def sonarServer = "http://10.0.0.11:9000"
     sh """
-    ${scannerHome}/bin/sonar-scanner -Dsonar.host.url=${sonarServer} \
-                                     -Dsonar.projectKey=${projectName} \
+    ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
                                      -Dsonar.projectName=${projectName} \
                                      -Dsonar.projectVersion=${version} \
-                                     -Dsonar.login=admin \
-                                     -Dsonar.password=xxzx@123 \
                                      -Dsonar.ws.timeout=30 \
                                      -Dsonar.projectDescription="${projectDescription}" \
                                      -Dsonar.sources="${projectPath}" \
