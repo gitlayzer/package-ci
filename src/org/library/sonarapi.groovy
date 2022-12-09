85c6b49ce1f88a3de2863c7b72e4a9d02303a1b6
gitlayzer <gitlayzer@kudevops.cn>
2022-12-09 06:52:14 -0500

    edit sonar code

---
+++ src/org/library/sonarapi.groovy
@@ -0,0 +1,26 @@
+package org.library
+
+// 封装HTTP请求
+def HttpRequest(requestType,requestUrl,requestBody) {
+    def sonarServer = "http://10.0.0.11:9000/api"
+    result = httpRequest authentication: 'sonar-admin-password',
+        httpMode: requestType,
+        contentType: "APPLICATION_JSON",
+        consoleLogResponseBody: true,
+        ignoreSslErrors: true,
+        requestBody: requestBody,
+        url: "${sonarServer}/${requestUrl}"
+    return result
+}
+
+
+// 获取Sonar项目状态
+def GetProjectStatus(projectName) {
+    apiUrl = "/qualitygates/project_status?projectKey=${projectName}"
+    response = HttpRequest("GET",apiUrl,'')
+
+    response = readJSON text: """${response.content}"""
+    println(response)
+
+    return response
+}
