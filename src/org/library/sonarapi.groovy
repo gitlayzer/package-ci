c577dbeac2d19ce285aa8834426b654e1b913a23
gitlayzer <gitlayzer@kudevops.cn>
2022-12-09 07:00:59 -0500

    edit sonar code

--- src/org/library/sonarapi.groovy
+++ src/org/library/sonarapi.groovy
@@ -16,7 +16,7 @@ def HttpRequest(requestType,requestUrl,requestBody) {
 
 // 获取Sonar项目状态
 def GetProjectStatus(projectName) {
-    apiUrl = "/qualitygates/project_status?projectKey=${projectName}"
+    apiUrl = "qualitygates/project_status?projectKey=${projectName}"
     response = HttpRequest("GET",apiUrl,'')
 
     response = readJSON text: """${response.content}"""
