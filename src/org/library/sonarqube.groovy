6929d9a034535de5f43a8d7baa07604ebc919672
gitlayzer <gitlayzer@kudevops.cn>
2022-12-11 03:16:48 -0500

    edit sonar code

--- src/org/library/sonarqube.groovy
+++ src/org/library/sonarqube.groovy
@@ -1,17 +1,34 @@
 package org.library
 
-def SonarScan(projectName,projectDescription,projectPath,version){
+def SonarScan(projectName,projectDescription,projectPath,version,runOps='',projectId='',commit_sha='',branchName=''){
     withSonarQubeEnv("sonarqube"){
         def scannerHome = "/jenkins/software/sonar-scanner/"
-        sh """
-        ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
-                                         -Dsonar.projectName=${projectName} \
-                                         -Dsonar.projectVersion=${version} \
-                                         -Dsonar.ws.timeout=30 \
-                                         -Dsonar.projectDescription="${projectDescription}" \
-                                         -Dsonar.sources="${projectPath}" \
-                                         -Dsonar.sourceEncoding=UTF-8 \
-                                         -Dsonar.java.binnaries=target/classes \
-        """
+        if (runOps == "GitlabPush") {
+            sh """
+                ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
+                                                 -Dsonar.projectName=${projectName} \
+                                                 -Dsonar.projectVersion=${version} \
+                                                 -Dsonar.ws.timeout=30 \
+                                                 -Dsonar.projectDescription="${projectDescription}" \
+                                                 -Dsonar.sources="${projectPath}" \
+                                                 -Dsonar.sourceEncoding=UTF-8 \
+                                                 -Dsonar.java.binnaries=target/classes \
+                                                 -Dsonar.analysis.mode=preview \
+                                                 -Dsonar.gitlab.project_id=${projectId} \
+                                                 -Dsonar.gitlab.commit_sha=${commit_sha} \
+                                                 -Dsonar.gitlab.ref_name=${branchName}
+                """
+        } else {
+            sh """
+                ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
+                                                 -Dsonar.projectName=${projectName} \
+                                                 -Dsonar.projectVersion=${version} \
+                                                 -Dsonar.ws.timeout=30 \
+                                                 -Dsonar.projectDescription="${projectDescription}" \
+                                                 -Dsonar.sources="${projectPath}" \
+                                                 -Dsonar.sourceEncoding=UTF-8 \
+                                                 -Dsonar.java.binnaries=target/classes
+                """
+        }
     }
 }
